61dd443cb0132cd264e933d3902770f18f0b9f6e
upendra <upendrasahu1199@gmail.com>
2023-02-09 12:16:59 +0530

    Day 11 And 12 Practice problem

---
+++ src/com/bridgelebz/Stock/StockAccountManagement.java
@@ -0,0 +1,37 @@
+package com.bridgelebz.Stock;
+
+import java.util.Scanner;
+
+public class StockAccountManagement {
+    public static void stockAccountManagement() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of Stock : ");
+        int nOfStock = scanner.nextInt();
+        scanner.nextLine();
+        String[] stockName = new String[nOfStock];
+        int nOfShare[] = new int[nOfStock];
+        double sharePrice[] = new double[nOfStock];
+        for (int i = 0; i < nOfStock; i++) {
+            System.out.print("Enter Stock " + (i + 1) + " Name : ");
+            stockName[i] = scanner.nextLine();
+            System.out.print("Enter Number of Shares : ");
+            nOfShare[i] = scanner.nextInt();
+            System.out.print("Enter Share Price : ");
+            sharePrice[i] = scanner.nextDouble();
+            scanner.nextLine();
+        }
+        double totalValue = 0.00;
+        System.out.println("Stock Report");
+//        System.out.println("Stock Name  " + "  Number of Share  " + "  Share Price  " + "  value");
+        for (int i = 0; i < nOfStock; i++) {
+            double stockValue = nOfShare[i] * sharePrice[i];
+            System.out.println("Stock Name : " + stockName[i] + " | Number of Share : " + nOfShare[i] + " | Share Price : " + sharePrice[i] + " | value : " + stockValue);
+            totalValue += stockValue;
+        }
+        System.out.println("Total value = " + totalValue);
+    }
+
+    public static void main(String[] args) {
+        stockAccountManagement();
+    }
+}
